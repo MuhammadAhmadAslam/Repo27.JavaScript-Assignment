8611bc055ccafbab2e6a542fa923aee6f23b889e
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-02-28 23:43:19 +0500

    Assignmnet 01

---
+++ app.js
@@ -0,0 +1,94 @@
+//CHAPTER 01 
+// Answer 01
+alert("Hello How Are You?");
+//Answer 02
+alert("Error! Please enter a valid passowrd");
+//Answer 03
+alert("Welcome To Programming Land\nHappy Coding");
+//Answer 04
+alert("Welcome To JS Land");
+//Answer 05
+console.log("Hello...I can run JS through my web browser console");
+
+
+// Chapter 02
+// Answer 01
+var userName= 'Ahmed';
+//Answer 02 
+var myName= "Muhammad Ahmed";
+//Answer 03
+var message= "Hello World";
+alert(message);
+//Answer 04
+var name= prompt('Enter Your Name');
+var age= prompt('Enter Your Age');
+var course= prompt('Enter Your Course');
+alert("NAME: " + name + "\nAge: " + age + "\nCourse " + course);
+//Answer 05
+alert("Pizza\nPizz\nPiz\nPi\nP");
+//Answer 06
+var emailtext= "Your Email Address is ";
+var email= "example@gmail.com";
+alert(emailtext + email);
+//Anwer 07
+var book= 'A smarter way to learn JavaScript';
+alert("I am trying to learn from the book " + book);
+//Anwer 08
+document.write("Yeah! I can write HTML content through Javascript");
+//Answer 09
+var emojis= '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+console.log(emojis);
+alert(emojis);
+
+
+
+//Chapter 03
+//Anwser 01
+var age= prompt('Enter Your Age');
+console.log("I am " + age + ' years old');
+//Answer 02
+var visited= 1;
+alert("You Have Visited the website " + visited + " time");
+//Answer 03
+var birthYear= 'I was born on 2007';
+document.write("<br>"+"\n" + birthYear);
+//Answer 04
+var name = prompt("Enter Your Name");
+alert('Name '+ name + '\nOdered: ' + 'Hoodies-Small' + '\nQuantity: ' + 5);
+
+
+//Chapter 04
+//Answer 01
+var variable= 10 , variable2=20 , variable3 = 30;
+//Answer 02
+document.write("<br>" + "For Legal Variable It Must Begin with letter , contain camelcase , contain only $ and _  symbols.");
+document.write("<br>" + "For Illegal Variable It cannot start with number , cannot contain symbols , cannot contain reserved keywords");
+//Answer 03
+//(a)
+document.write("<br>" + "Contain only letter, and doesnot start with number and used only special symbols like only $ and _");
+//(b)
+document.write("<br>" + "for answer b blanks:"+"<br>"+"For Legal Variable It Must Begin with letter , camelcase , and only $ and _  symbols.");
+//(c)
+document.write("<br>" + "for answer c blanks:" + "<br>" + "Variables must begin with a  letter , $ or _.");
+//(d)
+document.write("<br>" + "for answer d blanks: " + " Sensitive" );
+//(e)
+document.write(`<br>for answer e blanks:  Keywords<br>` );
+
+
+
+//Chapter 05
+//Answer 01
+var a= prompt("Enter Your Number");
+var b= prompt("Enter Your Number");
+var additionResult = a + b;
+document.write(  "Addition: " + additionResult + "<br>");
+
+var subtractionResult = a - b;
+document.write("Subtraction: " + subtractionResult + "<br>");
+
+var multiplicationResult = a * b;
+document.write("Multiplication: " + multiplicationResult + "<br>");
+
+var divisionResult = a / b;
+document.write("Divide: " + divisionResult + "<br>");
